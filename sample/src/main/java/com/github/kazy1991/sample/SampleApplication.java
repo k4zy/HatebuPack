d3c7bc54b3415001d0f323f870044119404e81f1
kazuki-yoshida <kazuki-yoshida@cookpad.com>
2017-01-25 19:51:01 +0900

    Initial Commit

---
+++ sample/src/main/java/com/github/kazy1991/sample/SampleApplication.java
@@ -0,0 +1,11 @@
+package com.github.kazy1991.sample;
+
+import android.app.Application;
+
+public class SampleApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
